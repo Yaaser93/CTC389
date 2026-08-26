a39dea0b4ac392feaae527d262c86711295adf29
Muhammad <yaaser.mahmood@gmail.com>
2026-08-26 18:05:11 +0000

    Lab #5

---
+++ mmahmood5.py
@@ -0,0 +1,39 @@
+#Muhammad Mahmood
+#Lab 5
+
+print("No Loop")
+num1 = 2
+num2 = 4
+num3 = 8
+num4 = 16
+num5 = 32
+num6 = 64
+num7 = 128
+num8 = 256
+num9 = 512
+num10 = 1024
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+print(num6)
+print(num7)
+print(num8)
+print(num9)
+print(num10)
+
+
+
+print("With Loop")
+nums = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+for i in nums:
+    print(i)
+
+
+print("Fruits and Vegetables")
+words = ["mango", "banana", "corn", "watermelon", "cherry"]
+for i in words:
+    print(i)
+
