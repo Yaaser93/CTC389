111575db4c5432c06aeaf418cb2872cb80a34652
Muhammad <yaaser.mahmood@gmail.com>
2026-09-10 00:53:18 +0000

    mmahmoodmid#2

---
+++ mmahmoodmid#2.py
@@ -0,0 +1,12 @@
+#Muhammad Mahmood
+#Midterm Question 2
+
+def rectarea(x,y):
+    area = x*y
+    return area
+
+base = int(input("What is the base of the rectangle? "))
+height = int(input("What is the height of the rectangle? "))
+response = rectarea(base,height)
+print("The area of the rectangle is",response)
+
