63999b8221604410766dd30858452c186f5bf0eb
Muhammad <yaaser.mahmood@gmail.com>
2026-09-10 00:23:44 +0000

    mmahmoodmid#1.py

---
+++ mmahmoodmid#1.py
@@ -0,0 +1,10 @@
+#Muhammad Mahmood
+#Midterm
+
+#Question 1
+
+name = str(input("Enter your name: "))
+number = int(input("Enter a number: "))
+newnumber = int(input("How much would you like to add to that number? "))
+
+print(name,", you entered",number,"and added",newnumber,"to get a total of", number+newnumber)
